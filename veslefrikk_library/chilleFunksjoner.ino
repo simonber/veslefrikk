954309cc55c775ad542b7d5fb2da39676b6558f9
eirikhtr <rockerumpah@gmail.com>
2016-03-08 11:03:42 +0100

    Update chilleFunksjoner.ino

--- veslefrikk_library/chilleFunksjoner.ino
+++ veslefrikk_library/chilleFunksjoner.ino
@@ -1,43 +1,45 @@
-double takeMeanValue(int sensorVec[], const int length) {
-  int tempSum = 0;
+#include "chilleFunksjoner.h"
+#include <math.h>
 
-  for (int i = 0; i < length; i++) {
-    tempSum += sensorVec[i];
-  }
-  return (double)tempSum / length;
+int takeMeanValue(int sensorVec[], const int length) {
+	int tempSum = 0;
+
+	for (int i = 0; i < length; i++) {
+		tempSum += sensorVec[i];
+	}
+	return (int)round(tempSum / (double)length);
 }
 
 void swap(int *sensorVec, int p1, int p2) {
-  int pitstop = sensorVec[p1];
-  sensorVec[p1] = sensorVec[p2];
-  sensorVec[p2] = pitstop;
+	int pitstop = sensorVec[p1];
+	sensorVec[p1] = sensorVec[p2];
+	sensorVec[p2] = pitstop;
 }
 
 
 void sort(int *sensorVec, const int length) {
 
-  for (int i = 0; i < (length-1); i++) {
-    for (int j = i + 1; j < length; j++) {
-      if (sensorVec[i] > sensorVec[j]) {
-        swap(sensorVec, i, j);
-      }
-    }
-  }
+	for (int i = 0; i < (length-1); i++) {
+		for (int j = i + 1; j < length; j++) {
+			if (sensorVec[i] > sensorVec[j]) {
+				swap(sensorVec, i, j);
+			}
+		}
+	}
 }
 
-double takeMedian(int *sensorVec, const int length) {
-  int res = 0;
-  sort(sensorVec, length);
+int takeMedian(int *sensorVec, const int length) {
+	int res = 0;
+	sort(sensorVec, length);
 
-  if (length % 2 == 0) {
-    res = (sensorVec[(length/2)-1] + sensorVec[length/2]) / 2;
-  }
-  else {
-    res = sensorVec[(length - 1) / 2];
-  }
-  return res;
+	if (length % 2 == 0) {
+		res = (int)round((sensorVec[(length/2)-1] + sensorVec[length/2]) / 2.0);
+	}
+	else {
+		res = sensorVec[(length - 1) / 2];
+	}
+	return res;
 }
-
 void tempWarning(int indoorTemp, int outdoorTemp) {
 
   if (indoorTemp-outdoorTemp > 20 || indoorTemp > 25) {
